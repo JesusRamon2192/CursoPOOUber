d322df39cfa634eb0943b4fc980954ffc90a1fd7
jesusRamon <jesus.ramon2192@gmail.com>
2022-12-04 13:37:02 -0600

    Clase 27 terminando herencias en php

--- PHP/index.php
+++ PHP/index.php
@@ -1,12 +1,13 @@
 <?php
 
-require_once 'account.php';
-require_once 'car.php' ;
+require_once("car.php");
+require_once("uberX.php");
+require_once("account.php");
+require_once("uberPool.php");
 
-$car1 = new Car("AW456", new Account("AndresHerrera", "AMS"));
+$car1 = new uberX("CVB123", new Account("Andres Herrera", "wqs123"), "Chevrolet", "Spark");
 $car1->PrintDataCar();
 
-$car2 = new Car("WAS123", new Account("Andrea Herrera", "MKS123"));
-$car2->PrintDataCar();
-
-?>
+$uberPool123 = new uberPool("QWE123", new Account("Andrea Medina", "POI123"), "Toyota", "Prius");
+$uberPool123->PrintDataCar();
+?> 
