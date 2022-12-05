66e107a22cb28cb4a0893b2f92888936043b98b8
jesusRamon <jesus.ramon2192@gmail.com>
2022-12-05 15:22:58 -0600

    Clase 28 Aplicando herencia en lenguaje Python y JavaScript

--- JS/Index.js
+++ JS/Index.js
@@ -1,3 +1,7 @@
 var car = new Car("AW456", new Account("Andres Herrera", "AWS123"))
-car.pasenger = 4;
+car.passenger = 4;
 car.printDataCar();
+
+var uberSpark = new UberX("JSH726", new Account("Andrea Ferran", "LKS987"), "Chevrolet", "Spark")
+uberSpark.passenger = 4;
+uberSpark.printDataCar();
